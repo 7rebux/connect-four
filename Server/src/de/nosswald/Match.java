b3ef47817b8b94ec6dad656b5dc5f07053b850a4
Kai Jellinghaus <contact@kaij.tech>
2022-05-18 22:51:43 +0200

    Fix checking

--- Server/src/de/nosswald/Match.java
+++ Server/src/de/nosswald/Match.java
@@ -28,7 +28,7 @@ public class Match {
 
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                if (board[col][row] == null)
+                if (board[col][row] != currentPlayer)
                     continue;
 
                 // horizontal
