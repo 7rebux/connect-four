2d9cf2c6a99b34ae7fd11c7f4c6815029346844e
Kai Jellinghaus <contact@kaij.tech>
2022-05-18 19:53:59 +0200

    Super basic Protocol

--- Client/src/de/nosswald/Board.java
+++ Client/src/de/nosswald/Board.java
@@ -4,19 +4,7 @@ package de.nosswald;
  * @author Nils Osswald
  */
 public final class Board {
-    /**
-     * Y: Yellow
-     * R: Red
-     * ' ': Empty
-     */
-
     public static final char Yellow = 'Y';
     public static final char Red = 'R';
     public static final char Empty = ' ';
-
-    private final char[][] board;
-
-    public Board(GameRules rules) {
-        board = new char[rules.getFieldWidth()][rules.getFieldHeight()];
-    }
 }
--- Client/src/de/nosswald/Main.java
+++ Client/src/de/nosswald/Main.java
@@ -11,8 +11,32 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) {
         GameRules g = new GameRules(6, 7);
-        Board b = new Board(g);
         Network n = new Network();
         GUI gui = new GUI(g);
+
+        n.addListener(new NetworkListener()
+        {
+            @Override
+            public void processMessage(byte[] message)
+            {
+                byte id = message[0];
+                switch(id) {
+                    case 0: {
+                        throw new RuntimeException("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                    }
+                    case 1: { // Tile Update
+                        byte row = message[1];
+                        byte col = message[2];
+                        byte newState = message[3];
+                        gui.onTileChange(row, col, newState == 0 ? ' ' : (newState == 1 ? Board.Yellow : Board.Red));
+                        break;
+                    }
+                    case 2: { // Win State
+                        boolean didIWin = message[1] == 1;
+                        throw new RuntimeException("TODO: IMPLEMENT WINNER SCREEN");
+                    }
+                }
+            }
+        });
     }
 }
