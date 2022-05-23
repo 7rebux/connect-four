b5744c6b830c2f2f8c7edc2983ddf8117b6c96a6
Kai Jellinghaus <contact@kaij.tech>
2022-05-23 06:48:41 +0200

    Tidy up UI

--- Client/src/de/nosswald/GUI.java
+++ Client/src/de/nosswald/GUI.java
@@ -37,8 +37,13 @@ public final class GUI extends JFrame {
         this.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                final int row = e.getY() / (GUI.this.getHeight() / rules.getFieldHeight());
-                final int col = e.getX() / (GUI.this.getHeight() / rules.getFieldHeight());
+                final int factor = Math.min(getHeight() / (rules.getFieldHeight() + 2), getWidth() / (rules.getFieldWidth() + 2));
+                final int row = (e.getY() / factor) - 1;
+                final int col = (e.getX() / factor) - 1;
+
+                if (row < 0 || row > rules.getFieldWidth() || col < 0 || col > rules.getFieldHeight()) {
+                    return;
+                }
 
                 System.out.printf("Pressed on [%d|%d] => [%d][%d]%n", e.getX(), e.getY(), col, row);
 
@@ -52,11 +57,10 @@ public final class GUI extends JFrame {
     @Override
     public void paint(Graphics g) {
         final Graphics2D g2d = (Graphics2D) g;
-        final int factor = getHeight() / rules.getFieldHeight();
+        // + 2 because we want to leave one empty row on each side
+        final int factor = Math.min(getHeight() / (rules.getFieldHeight() + 2), getWidth() / (rules.getFieldWidth() + 2));
 
-        super.paint(g2d);
-
-        g2d.setColor(Color.BLUE);
+        g2d.setColor(Color.GRAY);
         g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
 
         for (int row = 0; row < board[0].length; row++) {
@@ -67,7 +71,8 @@ public final class GUI extends JFrame {
                 else if (value == Board.Red) g2d.setColor(Color.RED);
                 else g2d.setColor(Color.WHITE);
 
-                g2d.fillOval(factor * row, factor * col, factor, factor);
+                // +1 on each axis for the top left padding
+                g2d.fillOval(factor * (row + 1), factor * (col + 1), factor, factor);
             }
         }
     }
